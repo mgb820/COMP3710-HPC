e00ad6f9f5eb58a338f7855c271494ba5be65a20
Hongkai Chen <hongkai.chen@hotmail.com>
2026-09-03 19:02:35 +1000

    updated unet job

--- unet_job.sh
+++ unet_job.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=unet_oasis
-#SBATCH --partition=comp3710
-#SBATCH --account=comp3710
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --job-name=oasis_unet
+#SBATCH --partition=a100
 #SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
 #SBATCH --time=02:00:00
 #SBATCH --output=unet_train_%j.out
 
