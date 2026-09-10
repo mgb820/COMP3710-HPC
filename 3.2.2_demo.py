946fec10606fb00ac22421ce6b0423d35c430325
Hongkai Chen <hongkai.chen@hotmail.com>
2026-09-10 21:08:49 +1000

    updated script

--- 3.2.2_demo.py
+++ 3.2.2_demo.py
@@ -16,7 +16,7 @@ transform = transforms.Compose([
 
 # 2. Load CIFAR-10 Test Dataset
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False)
+testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, num_workers=0, pin_memory=False)
 
 # 3. Load Saved Weights
 model = get_cifar_resnet18().to(device)
